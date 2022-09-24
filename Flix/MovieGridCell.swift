7a84402536f57805e4a2187247ff669cbf2dc57b
shivani-senguttuvan5 <104776549+shivani-senguttuvan5@users.noreply.github.com>
2022-09-23 20:14:56 -0500

    Add files via upload

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Shivani on 9/21/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
